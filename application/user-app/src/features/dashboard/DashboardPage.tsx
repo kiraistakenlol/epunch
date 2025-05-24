8b4d6f91e32afee610304cefdf81ca68b6e7cba1
Kira <kirill.sobolev.vl@gmail.com>
2025-05-24 17:36:51 -0300

    fix

--- application/user-app/src/features/dashboard/DashboardPage.tsx
+++ application/user-app/src/features/dashboard/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import UserQRCode from '../user/UserQRCode';
 import styles from './DashboardPage.module.css';
