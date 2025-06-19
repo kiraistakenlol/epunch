1a93b955dc2b3a7128ad37afccf856bb65bd49a1
Kira <kirill.sobolev.vl@gmail.com>
2025-06-19 18:32:23 -0300

    try fix no default icon 100 w and h

--- application/user-app/src/features/dashboard/punch-cards/punch-card/front/body/punches/punch-icon-circle/PunchIconCircleSvg.tsx
+++ application/user-app/src/features/dashboard/punch-cards/punch-card/front/body/punches/punch-icon-circle/PunchIconCircleSvg.tsx
@@ -13,6 +13,8 @@ const PunchIconCircleSvg: React.FC<PunchIconCircleSvgProps> = ({
   <div className={`${styles.punchIconContainer} ${className}`}>
     <svg
       className={styles.punchIconSvg}
+      width="100%"
+      height="100%"
       viewBox="0 0 100 100"
       xmlns="http://www.w3.org/2000/svg"
     >
