eb126d50df9e5adfae12b8f887e2499762ccc59f
Kira <kirill.sobolev.vl@gmail.com>
2025-06-27 21:08:13 -0300

    Remove introductory text from TopContactBar component for a cleaner UI.

--- application/merchant-app/src/pages/merchant-onboarding/components/TopContactBar.tsx
+++ application/merchant-app/src/pages/merchant-onboarding/components/TopContactBar.tsx
@@ -6,7 +6,6 @@ export const TopContactBar: React.FC = () => {
   return (
     <div className={styles.topContact}>
       <div className={styles.topContactContent}>
-        <span>Get started with ePunch: </span>
         <a href="https://wa.me/79250419362" className={styles.topContactLink}>
           <FaWhatsapp /> WhatsApp
         </a>
