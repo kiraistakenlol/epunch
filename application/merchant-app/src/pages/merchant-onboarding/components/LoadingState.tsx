8bdce14a42b7fe6238916481612b8c1676750cf8
Kira <kirill.sobolev.vl@gmail.com>
2025-07-01 09:00:12 -0300

    Add react-spinners for loading state and update LoadingState component styles

--- application/merchant-app/src/pages/merchant-onboarding/components/LoadingState.tsx
+++ application/merchant-app/src/pages/merchant-onboarding/components/LoadingState.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { useI18n } from 'e-punch-common-ui';
-import { EpunchSpinner } from '../../../components/foundational';
+import { PulseLoader } from 'react-spinners';
 import styles from './LoadingState.module.css';
 
 export const LoadingState: React.FC = () => {
-  const { t } = useI18n('merchantOnboarding');
-  
   return (
     <div className={styles.loadingContainer}>
-      <EpunchSpinner size="lg" />
-      <p>{t('loading.merchantInformation')}</p>
+      <PulseLoader size={15} color="#333" />
     </div>
   );
 }; 
