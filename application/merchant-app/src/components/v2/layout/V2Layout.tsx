ae07941529dc8eff5240795a869d9e57bf36215f
Kira <kirill.sobolev.vl@gmail.com>
2025-07-02 14:32:31 -0300

    Remove unused breadcrumbs from V2Layout component

--- application/merchant-app/src/components/v2/layout/V2Layout.tsx
+++ application/merchant-app/src/components/v2/layout/V2Layout.tsx
@@ -2,13 +2,8 @@ import { Outlet } from 'react-router-dom';
 import { AppShell } from './AppShell';
 
 export function V2Layout() {
-  // The breadcrumbs can be dynamic based on the route in a real app
-  const breadcrumbs = [
-    { label: 'V2', isCurrentPage: true },
-  ];
-
   return (
-    <AppShell breadcrumbs={breadcrumbs}>
+    <AppShell>
       <Outlet />
     </AppShell>
   );
